e7fcee5f6851876c944b094eef3a775437e8cd5f
julian_lev <julian.levkov@epfl.ch>
2025-11-12 20:03:08 +0100

    update server

--- Lab 3/secure_pmu.py
+++ Lab 3/secure_pmu.py
@@ -2,7 +2,6 @@ import socket
 import argparse
 import ssl
 
-
 def main():
     # Create the parser
     parser = argparse.ArgumentParser(description='Process some command line arguments.')
@@ -11,10 +10,6 @@ def main():
     parser.add_argument('port', type=int, help='The port number')
     parser.add_argument('certificate', type=str, help='The path to cert')
     parser.add_argument('key', type=str, help='The path to key')
-
-    # port= 5003
-    # path to certificate= /cert/output/certificate.crt
-    # path to key= /343473_key.pem
 
     # Parse the arguments
     args = parser.parse_args()
@@ -26,30 +21,36 @@ def main():
 
     hostname = '127.0.0.1'
 
-    # Create the ssl context
+    # Create the SSL context
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    context.load_cert_chain(certificate, key)
+    context.load_cert_chain(certfile=certificate, keyfile=key)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
+    # Create and bind the socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.bind((hostname, port))
         sock.listen(5)
 
-        with context.wrap_socket(sock, server_side=True) as ssock:
-            while True:
-                conn, addr = ssock.accept()            
-                data = conn.recv(11)
-                if data.decode() == "CMD_short:0":
-                    conn.sendall("This is PMU data 0".encode())
-                    conn.sendall("This is PMU data 1".encode())
-                    conn.sendall("This is PMU data 2".encode())
-                    conn.sendall("This is PMU data 3".encode())
-                    conn.close()
+        print(f'Server listening on {hostname}:{port}...')
 
-                else:
-                    conn.close()
-            
+        while True:
+            try:
+                # Accept a connection
+                conn, addr = sock.accept()
+                print(f'Connection from {addr} has been established.')
+
+                # Wrap the accepted socket with SSL
+                with context.wrap_socket(conn, server_side=True) as ssock:
+                    data = ssock.recv(1024)  # Increased buffer size to 1024 bytes
+                    if data.decode() == "CMD_short:0":
+                        # Send response messages
+                        for i in range(4):  # Sending 4 messages
+                            ssock.sendall(f"This is PMU data {i}".encode())
+                    else:
+                        print(f'Unknown command received: {data.decode()}')
+            except Exception as e:
+                print(f'Error occurred: {e}')
+            finally:
+                conn.close()  # Ensure the connection is closed
 
 if __name__ == '__main__':
     main()
-
-
